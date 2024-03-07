57c2698dab9decd1905b9b969a4332207022bfaa
eugenio-tesio-ueno <eugenio.tesio@itti.digital>
2024-03-07 09:52:08 -0300

    feat: add dart app and workflow to test this action

---
+++ test/counter_test.dart
@@ -0,0 +1,13 @@
+// Import the test package and Counter class
+import 'package:flutter_action/counter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Counter value should be incremented', () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+}
