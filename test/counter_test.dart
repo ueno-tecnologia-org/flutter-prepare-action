1721784f3b82edae7768db2937facf365b931bb5
eugenio-tesio-ueno <eugenio.tesio@itti.digital>
2024-05-03 09:51:50 -0300

    fix: add commented test

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -1,13 +1,13 @@
-// // Import the test package and Counter class
-// import 'package:flutter_action/counter.dart';
-// import 'package:test/test.dart';
+// Import the test package and Counter class
+import 'package:flutter_action/counter.dart';
+import 'package:test/test.dart';
 
-// void main() {
-//   test('Counter value should be incremented', () {
-//     final counter = Counter();
+void main() {
+  test('Counter value should be incremented', () {
+    final counter = Counter();
 
-//     counter.increment();
+    counter.increment();
 
-//     expect(counter.value, 1);
-//   });
-// }
+    expect(counter.value, 1);
+  });
+}
